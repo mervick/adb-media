a9aa5486c929979598d1a4be5f62421e6e3de34a
Andrey Izman <izmanw@gmail.com>
2020-12-07 15:39:04 +0200

    Update media

--- media.sh
+++ media.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
-adb=/home/izman/Android/Sdk/platform-tools/adb
+adb=~/Android/Sdk/platform-tools/adb
 notify=false
 driver='notify-send'
-#app='org.telegram.messenger'
-app='com.musicplayer.blackplayerfree'
 icon='audio-headphones'
 
 if [[ "${1::1}" != '-' ]]; then
@@ -21,10 +19,10 @@ DESCRIPTION
 
 USAGE
     bash play.sh <command> [-n[=<driver>]|--notification[=<driver>]]
-      [-a <app>|-id <app> --app=<app>] [-i <icon>|--icon=<icon>] [-h|--help]
+      [-a <app>|-id <app>|--app=<app>] [-i <icon>|--icon=<icon>] [-h|--help]
 
 ARGUMENTS
-    command (play|pause|stop|next|prev|rewind|forward|mute|up|down)
+    command (play|pause|stop|next|prev|rewind|forward|mute|up|down|info)
       play      - play/pause song
       pause     - play/pause song
       stop      - stop paying song
@@ -35,6 +33,7 @@ ARGUMENTS
       mute      - mute/unmute sound
       up        - set volume up
       down      - set volume down
+      info      - get current media info
 
 OPTIONS
     -n[=<driver>], --notification[=<driver>]
@@ -42,7 +41,7 @@ OPTIONS
       'notify-send'. By default is '${driver}'
 
     -a <app>, -id <app>, --app=<app>
-      Select notification application. By default is '${app}'
+      Select notification application. By default detects last played application'
 
     -i <icon>, --icon=<icon>
       Notification icon. Default is '${icon}'
@@ -90,6 +89,13 @@ EOM
   shift
 done
 
+if [[ ! $app ]]; then
+  re='Audio playback\s[^¤]*packages=([a-zA-Z0-9\.\-]+)'
+  dump="$($adb shell dumpsys media_session)"
+  [[ $? != 0 ]] && exit 10
+  app="$(echo "$dump" | perl -n00e 's/'"$re"'/\$1/m && print "$1";')"
+fi
+
 volume=false
 
 if [[ "$command" == "" ]]; then
@@ -97,30 +103,26 @@ if [[ "$command" == "" ]]; then
     exit 7
 else
   case "$command" in
-    play)     code=85 ;; # 85 -->  "KEYCODE_MEDIA_PLAY_PAUSE"
-    pause)    code=85 ;; # 85 -->  "KEYCODE_MEDIA_PLAY_PAUSE"
-    stop)     code=86 ;; # 86 -->  "KEYCODE_MEDIA_STOP"
-    next)     code=87 ;; # 87 -->  "KEYCODE_MEDIA_NEXT"
-    prev)     code=88 ;; # 88 -->  "KEYCODE_MEDIA_PREVIOUS"
-    forward)  code=90 ;; # 90 -->  "KEYCODE_MEDIA_FAST_FORWARD"
-    rewind)   code=89 ;; # 89 -->  "KEYCODE_MEDIA_REWIND"
-    mute)     code=91 ;; # 91 -->  "KEYCODE_MUTE"
-    up)       code=24
-              volume=true
-    ;;
-    down)     code=25
-              volume=true
-    ;;
+    play)     code=85;; # 85 -->  "KEYCODE_MEDIA_PLAY_PAUSE"
+    pause)    code=85;; # 85 -->  "KEYCODE_MEDIA_PLAY_PAUSE"
+    stop)     code=86;; # 86 -->  "KEYCODE_MEDIA_STOP"
+    next)     code=87;; # 87 -->  "KEYCODE_MEDIA_NEXT"
+    prev)     code=88;; # 88 -->  "KEYCODE_MEDIA_PREVIOUS"
+    forward)  code=90;; # 90 -->  "KEYCODE_MEDIA_FAST_FORWARD"
+    rewind)   code=89;; # 89 -->  "KEYCODE_MEDIA_REWIND"
+    mute)     code=91;; # 91 -->  "KEYCODE_MUTE"
+    up)       code=24; volume=true;;
+    down)     code=25; volume=true;;
+    info)     info=true;;
     *)
       (1>&2 echo "Invalid command $command")
       exit 8
   esac
 fi
 
-$adb shell input keyevent "$code"
-echo $command $code $(whoami) >> /tmp/out
+[[ ! $info ]] && $adb shell input keyevent "$code"
 
-if ${notify}; then
+if ${notify} || [[ $info ]] ; then
   SOURCE="${BASH_SOURCE[0]}"
   while [[ -h "$SOURCE" ]]; do
     DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
@@ -129,32 +131,41 @@ if ${notify}; then
   done
   DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
 
-  if [[ "$driver" != "" ]]; then
+  if [[ "$driver" || $info ]]; then
     if ${volume}; then
-      volume=$(bash "$DIR/get-volume-level.sh" -d=headset -p)
-      err=$?
+      volume=$(bash "$DIR/get-volume-level.sh" -d=headset -p); err=$?
       [[ $err != 0 ]] && exit $err
       notify_args=("Android volume $volume%" -i "$icon")
     else
       sleep .5
-      data="$(bash "$DIR/get-notification.sh" --app="$app" -d ' ### ')"
-      err=$?
+      data="$(bash "$DIR/get-notification.sh" --app="$app" -d ' ### ')"; err=$?
       [[ $err != 0 ]] && exit $err
       title="${data##* ### }"
       [[ "$title" == "" ]] && exit 0
-      text="${data%% ### *}"
-      if [[ "$text" != "$title" ]]; then
-        notify_args=("$text" "$title" -i "$icon")
-      else
-        notify_args=("$title" -i "$icon")
+      artist="${data%% ### *}"
+
+      if ${notify}; then
+        readarray -t data <<< "$(php "$DIR/parse-rhytmbox.php" "$artist" "$title")"
+
+        if [[ "${data[3]}" ]]; then
+          icon="${data[3]}"
+        fi
+        if [[ "${data[2]}" ]]; then
+          title="$title\n<i>from</i> ${data[2]}"
+        fi
       fi
+      notify_args=("$artist" "$title" -i "$icon")
     fi
   fi
 
-  case "$driver" in
-    notify-send)
-      pkill notify-osd
-      notify-send "${notify_args[@]}"
-    ;;
-  esac
+  if ${notify}; then
+    case "$driver" in
+      notify-send)
+        pkill notify-osd
+        notify-send "${notify_args[@]}"
+      ;;
+    esac
+  elif ${info}; then
+    echo "$artist - $title"
+  fi
 fi
